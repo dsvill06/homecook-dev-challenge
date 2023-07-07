46dbb62abe60841c2d7ce9222259ff4c25ecbdf2
David Villavicencio <111402717+dsvill06@users.noreply.github.com>
2023-07-07 14:39:19 -0400

    Added navigation to home through logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
     <div class="bg-primary-200 h-12 w-full">
       <h1>
-      <img src="https://homecookapp.com/static/media/homecook_logo.1ca10013af71605db8e8.png"
-        alt="Kitchen Logo"
-        class="w-12 pt-1"/>
+        <a href="/">
+          <img
+            src="https://homecookapp.com/static/media/homecook_logo.1ca10013af71605db8e8.png"
+            alt="Kitchen Logo"
+            class="w-16 "
+          />
+        </a>
       </h1>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
